5371faa296e102c539fbfda5be3de3256a9d50e9
ANUBHAV DIXIT <139374541+Anubhav7525@users.noreply.github.com>
2026-02-10 12:35:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,151 @@
+
+
+const slides = [
+  {
+  title: "Taj Mahal",
+  subtitle: "Agra",
+  description: "Beuty Of Love",
+  image: "taj mahal .png.jfif"
+  },
+  {
+    title: "JaiPur City Palace",
+  subtitle: "Rajasthan",
+  description: "A royal palace complex showcasing the grandeur of India’s cultural heritage.",
+  image: "City palace jaipur rajasthan india _ Premium Photo.jfif"
+  },
+  {
+    title: "Varanasi Ghats",
+  subtitle: "Varanashi",
+  description: "Ancient riverfront ghats reflecting India’s deep spiritual traditions",
+  image: "Sunrise Over Varanasi Ghats.jfif"
+  },
+  {
+   title: "Goa Beaches",
+  subtitle: "Sun & Sea Paradise",
+  description: "opular coastal beaches known for vibrant culture and scenic beauty.",
+  image: "Goa Fitness Escape - Sunset Beach Fitness Dreams.jfif"
+  },
+  {
+   title: "Kerala Backwaters",
+  subtitle: "God’s Own Country",
+  description: "Peaceful waterways surrounded by lush greenery and natural charm.",
+  image: "kerala green.jfif"
+  }
+];
+
+function useTilt(active) {
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!ref.current || !active) {
+      return;
+    }
+
+    const state = {
+      rect: undefined,
+      mouseX: undefined,
+      mouseY: undefined
+    };
+
+    let el = ref.current;
+
+    const handleMouseMove = (e) => {
+      if (!el) {
+        return;
+      }
+      if (!state.rect) {
+        state.rect = el.getBoundingClientRect();
+      }
+      state.mouseX = e.clientX;
+      state.mouseY = e.clientY;
+      const px = (state.mouseX - state.rect.left) / state.rect.width;
+      const py = (state.mouseY - state.rect.top) / state.rect.height;
+
+      el.style.setProperty("--px", px);
+      el.style.setProperty("--py", py);
+    };
+
+    el.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      el.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [active]);
+
+  return ref;
+}
+
+const initialState = {
+  slideIndex: 0
+};
+
+const slidesReducer = (state, event) => {
+  if (event.type === "NEXT") {
+    return {
+      ...state,
+      slideIndex: (state.slideIndex + 1) % slides.length
+    };
+  }
+  if (event.type === "PREV") {
+    return {
+      ...state,
+      slideIndex:
+        state.slideIndex === 0 ? slides.length - 1 : state.slideIndex - 1
+    };
+  }
+};
+
+function Slide({ slide, offset }) {
+  const active = offset === 0 ? true : null;
+  const ref = useTilt(active);
+
+  return (
+    <div
+      ref={ref}
+      className="slide"
+      data-active={active}
+      style={{
+        "--offset": offset,
+        "--dir": offset === 0 ? 0 : offset > 0 ? 1 : -1
+      }}
+    >
+      <div
+        className="slideBackground"
+        style={{
+          backgroundImage: `url('${slide.image}')`
+        }}
+      />
+      <div
+        className="slideContent"
+        style={{
+          backgroundImage: `url('${slide.image}')`
+        }}
+      >
+        <div className="slideContentInner">
+          <h2 className="slideTitle">{slide.title}</h2>
+          <h3 className="slideSubtitle">{slide.subtitle}</h3>
+          <p className="slideDescription">{slide.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [state, dispatch] = React.useReducer(slidesReducer, initialState);
+
+  return (
+    <div className="slides">
+      <button onClick={() => dispatch({ type: "PREV" })}>‹</button>
+
+      {[...slides, ...slides, ...slides].map((slide, i) => {
+        let offset = slides.length + (state.slideIndex - i);
+        return <Slide slide={slide} offset={offset} key={i} />;
+      })}
+      <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+    </div>
+  );
+}
+
+const elApp = document.getElementById("app");
+ReactDOM.render(<App />, elApp);
